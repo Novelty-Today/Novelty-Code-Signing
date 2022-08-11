ad2736d8fcd270880df7693c7aa834d0fd207c6f
nkoreli <58192451+nkoreli@users.noreply.github.com>
2022-08-11 18:27:13 +0400

    refactor

--- front/src/functions/signAndVerify.ts
+++ front/src/functions/signAndVerify.ts
@@ -10,90 +10,54 @@ export const onSubmitSign = async (
   setVerificationKey: (arg: SetStateAction<string>) => void,
   e: FormEvent
 ) => {
-  e.preventDefault();
-  if (
-    !fileToSignRef.current!.files ||
-    fileToSignRef.current!.files.length === 0
-  ) {
-    alert("No file selected, please select a file to sign");
-    return;
-  }
-  if (fileToSignRef.current!.files && fileToSignRef.current!.files.length > 1) {
-    alert("Too many files selected, please select only a single file");
-    return;
-  }
-  const address: string = await checkWebProviderAndConnect();
-  if(!address) return
-
-  const buffer = await makeBufferFromFile(fileToSignRef.current?.files?.[0] as File);
-
-  const msg = `0x${buffer.toString("hex")}`;
-
   try {
+    e.preventDefault();
+    fileWarning(fileToSignRef);
+
+    const address: string = await checkWebProviderAndConnect();
+    if (!address) return;
+
+    const buffer = await makeBufferFromFile(
+      fileToSignRef.current?.files?.[0] as File
+    );
+
+    const msg = `0x${buffer.toString("hex")}`;
+
     const signature: string = await window.ethereum.request({
       method: "personal_sign",
       params: [msg, address, ""],
     });
     if (signature) {
       const response = await addSignature({
-        filename: fileToSignRef.current!.files[0].name,
+        filename: fileToSignRef.current!.files?.[0].name as string,
         signature,
         timestamp: new Date().toUTCString(),
         userAddress: address,
       });
       setVerificationKey(response.tokenId);
-    } else {
-      alert("The data was not signed");
-    }
-  } catch (err: any) {
-    console.log(err.message);
-    alert("Something went wrong while signing file content");
+    } else throw Error("The data was not signed");
+  } catch (error: any) {
+    alert(error?.message);
   }
 };
 
 const checkWebProviderAndConnect = async () => {
-  try {
-    const provider = await detectEthereumProvider(); // just gives true/false, no UI involved
+  const provider = await detectEthereumProvider(); // just gives true/false, no UI involved
 
-    if (!provider) {
-      alert("Wallet not found. Please install MetaMask.");
-    } else {
-      const tempAddress = await getWalletAddress();
-      return tempAddress;
-    }
-  } catch (error: any) {
-    alert(error?.message || "Something went wrong while connecting to wallet.");
+  if (!provider) {
+    throw Error("Wallet not found. Please install MetaMask.");
+  } else {
+    await window?.ethereum?.enable(); // connect action is done here
+    const tempAddress = await getWalletAddress();
+    return tempAddress;
   }
 };
 
 const getWalletAddress = async () => {
-  try {
-    await window?.ethereum?.enable(); // connect action is done here
-    const accounts = await window?.ethereum?.request({
-      method: "eth_requestAccounts",
-    }); // gets all account
-    return accounts[0];
-  } catch (error: any) {
-    if (error?.code === 4001) {
-      // If this happens, the user rejected the connection request.
-      alert("Rejected Request, Please connect to MetaMask.");
-    } else if (error?.code === -32002) {
-      alert("User Request Pending, Please unlock MetaMask and try agin.");
-    } else {
-      console.log(error);
-    }
-    return null;
-  }
-};
-
-const getHashFromURI = (URI: string) => {
-  const lastIndex = URI.lastIndexOf("/");
-  return URI.slice(lastIndex + 1);
-};
-
-const makeBufferFromFile = async (file: File) => {
-  const dataArray = await file.arrayBuffer();
-  return Buffer.from(dataArray);
+  const accounts = await window?.ethereum?.request({
+    method: "eth_requestAccounts",
+  }); // gets all account
+  return accounts[0];
 };
 
 export const onSubmitVerify = async (
@@ -101,38 +65,26 @@ export const onSubmitVerify = async (
   verificationKey: string,
   e: FormEvent
 ) => {
-  e.preventDefault();
-  if (!/^[0-9]+$/.test(verificationKey)) {
-    alert("Invalid verification key");
-    return;
-  }
-  if (
-    !fileToVerifyRef.current!.files ||
-    fileToVerifyRef.current!.files.length === 0
-  ) {
-    alert("No file selected, please select a file to sign");
-    return;
-  }
-  if (
-    fileToVerifyRef.current!.files &&
-    fileToVerifyRef.current!.files.length > 1
-  ) {
-    alert("Too many files selected, please select only a single file");
-  }
-
-  const response = await getTokenURI(parseInt(verificationKey));
-
-  if (response.status === "error") {
-    alert("Invalid verification key");
-    return;
-  }
-  const ipfsHash = getHashFromURI(response.URI);
-  const ipfsResponse = (await axios.get(`https://dweb.link/ipfs/${ipfsHash}`))
-    .data;
-
-  const buffer = await makeBufferFromFile(fileToVerifyRef.current?.files?.[0] as File);
-  const msg = `0x${buffer.toString("hex")}`;
   try {
+    e.preventDefault();
+    if (!/^[0-9]+$/.test(verificationKey)) {
+      throw Error("Invalid verification key");
+    }
+    fileWarning(fileToVerifyRef);
+
+    const response = await getTokenURI(parseInt(verificationKey));
+
+    if (response.status === "error") throw Error("Invalid verification key");
+
+    const ipfsHash = getHashFromURI(response.URI);
+    const ipfsResponse = (await axios.get(`https://dweb.link/ipfs/${ipfsHash}`))
+      .data;
+
+    const buffer = await makeBufferFromFile(
+      fileToVerifyRef.current?.files?.[0] as File
+    );
+    const msg = `0x${buffer.toString("hex")}`;
+    await window?.ethereum?.enable()
     const signedAddress = await window.ethereum.request({
       method: "personal_ecRecover",
       params: [msg, ipfsResponse.signature],
@@ -141,8 +93,33 @@ export const onSubmitVerify = async (
       alert(
         `Verification succeeded!\n${signedAddress} has signed this file.\n`
       );
-    else alert("Verification failed");
-  } catch (e) {
-    alert("Verification failed");
+    else throw Error("Verification failed");
+  } catch (error: any) {
+    alert(error?.message);
+  }
+};
+
+const makeBufferFromFile = async (file: File) => {
+  const dataArray = await file.arrayBuffer();
+  return Buffer.from(dataArray);
+};
+
+const getHashFromURI = (URI: string) => {
+  const lastIndex = URI.lastIndexOf("/");
+  return URI.slice(lastIndex + 1);
+};
+
+const fileWarning = (fileToVerifyRef: RefObject<HTMLInputElement>) => {
+  if (
+    !fileToVerifyRef.current!.files ||
+    fileToVerifyRef.current!.files.length === 0
+  ) {
+    throw Error("No file selected, please select a file to sign");
+  }
+  if (
+    fileToVerifyRef.current!.files &&
+    fileToVerifyRef.current!.files.length > 1
+  ) {
+    throw Error("Too many files selected, please select only a single file");
   }
 };
