e9f69853d9aaeb6d28adab6c8cf17f56d3e78d75
nkoreli <58192451+nkoreli@users.noreply.github.com>
2022-08-11 15:13:52 +0400

    refactor

--- front/src/functions/signAndVerify.ts
+++ front/src/functions/signAndVerify.ts
@@ -1,4 +1,3 @@
-
 import { FormEvent, SetStateAction, RefObject } from "react";
 import { Buffer } from "buffer";
 import detectEthereumProvider from "@metamask/detect-provider";
@@ -7,56 +6,59 @@ import { addSignature } from "../API/addSignature";
 import { getTokenURI } from "../API/getTokenURI";
 
 export const onSubmitSign = async (
-    fileToSignRef: RefObject<HTMLInputElement>,
-    setVerificationKey: (arg: SetStateAction<string>) => void,
-    e: FormEvent
-  ) => {
-    e.preventDefault();
-    if (
-      !fileToSignRef.current!.files ||
-      fileToSignRef.current!.files.length === 0
-    ) {
-      alert("No file selected, please select a file to sign");
-      return;
-    }
-    if (fileToSignRef.current!.files && fileToSignRef.current!.files.length > 1) {
-      alert("Too many files selected, please select only a single file");
-      return;
-    }
-    const address: string = await checkWebProviderAndConnect();
-    const dataArray = await fileToSignRef.current!.files[0].arrayBuffer();
-    const buffer = Buffer.from(dataArray);
-    const msg = `0x${buffer.toString("hex")}`;
-    try {
-      const signature: string = await window.ethereum.request({
-        method: "personal_sign",
-        params: [msg, address, ""],
+  fileToSignRef: RefObject<HTMLInputElement>,
+  setVerificationKey: (arg: SetStateAction<string>) => void,
+  e: FormEvent
+) => {
+  e.preventDefault();
+  if (
+    !fileToSignRef.current!.files ||
+    fileToSignRef.current!.files.length === 0
+  ) {
+    alert("No file selected, please select a file to sign");
+    return;
+  }
+  if (fileToSignRef.current!.files && fileToSignRef.current!.files.length > 1) {
+    alert("Too many files selected, please select only a single file");
+    return;
+  }
+  const address: string = await checkWebProviderAndConnect();
+  if(!address) return
+
+  const buffer = await makeBufferFromFile(fileToSignRef.current?.files?.[0] as File);
+
+  const msg = `0x${buffer.toString("hex")}`;
+
+  try {
+    const signature: string = await window.ethereum.request({
+      method: "personal_sign",
+      params: [msg, address, ""],
+    });
+    if (signature) {
+      const response = await addSignature({
+        filename: fileToSignRef.current!.files[0].name,
+        signature,
+        timestamp: new Date().toUTCString(),
+        userAddress: address,
       });
-      if (signature) {
-        const response = await addSignature({
-          filename: fileToSignRef.current!.files[0].name,
-          signature,
-          timestamp: new Date().toUTCString(),
-          userAddress: address,
-        });
-        setVerificationKey(response.tokenId);
-      } else {
-        alert("The data was not signed");
-      }
-    } catch (err: any) {
-      console.log(err.message);
-      alert("Something went wrong while signing file content");
+      setVerificationKey(response.tokenId);
+    } else {
+      alert("The data was not signed");
     }
-  };
+  } catch (err: any) {
+    console.log(err.message);
+    alert("Something went wrong while signing file content");
+  }
+};
 
 const checkWebProviderAndConnect = async () => {
   try {
-    const provider = await detectEthereumProvider();
+    const provider = await detectEthereumProvider(); // just gives true/false, no UI involved
 
     if (!provider) {
       alert("Wallet not found. Please install MetaMask.");
     } else {
-      const tempAddress = await metamaskConnect();
+      const tempAddress = await getWalletAddress();
       return tempAddress;
     }
   } catch (error: any) {
@@ -64,13 +66,12 @@ const checkWebProviderAndConnect = async () => {
   }
 };
 
-const metamaskConnect = async () => {
+const getWalletAddress = async () => {
   try {
-    await window?.ethereum?.enable();
+    await window?.ethereum?.enable(); // connect action is done here
     const accounts = await window?.ethereum?.request({
       method: "eth_requestAccounts",
-    });
-
+    }); // gets all account
     return accounts[0];
   } catch (error: any) {
     if (error?.code === 4001) {
@@ -90,7 +91,10 @@ const getHashFromURI = (URI: string) => {
   return URI.slice(lastIndex + 1);
 };
 
-
+const makeBufferFromFile = async (file: File) => {
+  const dataArray = await file.arrayBuffer();
+  return Buffer.from(dataArray);
+};
 
 export const onSubmitVerify = async (
   fileToVerifyRef: RefObject<HTMLInputElement>,
@@ -115,7 +119,9 @@ export const onSubmitVerify = async (
   ) {
     alert("Too many files selected, please select only a single file");
   }
+
   const response = await getTokenURI(parseInt(verificationKey));
+
   if (response.status === "error") {
     alert("Invalid verification key");
     return;
@@ -123,9 +129,8 @@ export const onSubmitVerify = async (
   const ipfsHash = getHashFromURI(response.URI);
   const ipfsResponse = (await axios.get(`https://dweb.link/ipfs/${ipfsHash}`))
     .data;
-  console.log(ipfsResponse);
-  const dataArray = await fileToVerifyRef.current!.files[0].arrayBuffer();
-  const buffer = Buffer.from(dataArray);
+
+  const buffer = await makeBufferFromFile(fileToVerifyRef.current?.files?.[0] as File);
   const msg = `0x${buffer.toString("hex")}`;
   try {
     const signedAddress = await window.ethereum.request({
@@ -141,4 +146,3 @@ export const onSubmitVerify = async (
     alert("Verification failed");
   }
 };
-
