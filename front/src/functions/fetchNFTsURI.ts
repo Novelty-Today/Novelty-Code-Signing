a791612ca7e27d1e7fa9c0a702d7333516d9ffb3
Ar1stotele <20100274@ibsu.edu.ge>
2022-08-13 11:03:04 +0400

    cleaned code

--- front/src/functions/fetchNFTsURI.ts
+++ front/src/functions/fetchNFTsURI.ts
@@ -1,6 +1,21 @@
+import { getNFTsMetadata } from "../API/getNFTsMetadata";
 import { getNFTsURI } from "../API/getNFTsURI";
 
-export const fetchNFTsURI = async (userWalletAddress: string) => {
-  const ipfsURLs = await getNFTsURI(userWalletAddress);
-  console.log(ipfsURLs);
+export const fetchNFTsURI = async (
+  userWalletAddress: string,
+  setIsLoading: (param: boolean) => void,
+  setNftUri: (param: any[]) => void
+) => {
+  setIsLoading(true);
+  const ipfsURLs = await getNFTsURI(userWalletAddress || "");
+  if (ipfsURLs) {
+    let userNFTsMetadata = [];
+    for (let i = 0; i < ipfsURLs.length; ++i) {
+      const data = await getNFTsMetadata(ipfsURLs[i]);
+      userNFTsMetadata.push(data);
+    }
+    setNftUri(userNFTsMetadata);
+    console.log(userNFTsMetadata);
+    setIsLoading(false);
+  }
 };
