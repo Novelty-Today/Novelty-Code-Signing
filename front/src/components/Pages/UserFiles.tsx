91a207af7bb70585874800ea597063e3b643cb2e
Ar1stotele <20100274@ibsu.edu.ge>
2022-08-13 11:07:51 +0400

    Added important to the spinner size

--- front/src/components/Pages/UserFiles.tsx
+++ front/src/components/Pages/UserFiles.tsx
@@ -19,7 +19,7 @@ export const UserFiles = () => {
       <div className="min-w-[300px] w-[80%] mx-auto p-4 border-[1px] border-[#6d6d6d3b] rounded-[6px] shadow-[2px_2px_10px_0px_rgba(0,0,0,0.1)] min-h-[240px]">
         {isLoading && (
           <div className="flex justify-center items-center">
-            <Spinner styleSize="mt-[2.5%] w-[45px] h-[45px]" />
+            <Spinner styleSize="mt-[2.5%] !w-[45px] !h-[45px]" />
           </div>
         )}
         {nftUri &&
