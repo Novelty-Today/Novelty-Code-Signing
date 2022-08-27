9b93c800416b937f1784e7cd358aaf8114b2bc52
giorgi-kiknavelidze <69247736+giorgi-kiknavelidze@users.noreply.github.com>
2022-08-27 12:21:59 +0400

    split signature in python and other misc changes

---
+++ email-verifier/src/Components/Pages/Success.tsx
@@ -0,0 +1,7 @@
+export const Success = () => {
+	return (
+		<div className="flex flex-col items-center mt-4 text-4xl font-bold text-[#3d61ff]">
+			Success! you may now close this page.
+		</div>
+	);
+};
