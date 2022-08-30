7c498bbf1b2908ffae646720b1a9d1060e139788
Ar1stotele <87176560+Ar1stotele@users.noreply.github.com>
2022-08-30 15:10:49 +0400

    back && blockchain - wired up

--- back/src/constants.ts
+++ back/src/constants.ts
@@ -1,2 +1,12 @@
-export const contractAddress = "0x4ec410f93686df69da5629532d60a8Ec97725059";
-// "0xD240d3C19E45B9D947F1E46D89F5e67090134D09";
+export const contractAddress = "0x961c1584772A7699bE5f15D20fB915ca9Ab0a25A";
+// "0x4ec410f93686df69da5629532d60a8Ec97725059";
+
+export const IdentityContractAddress =
+  "0xB958160ffe81D8D62C3C21d4E579627590dd7542";
+
+export const signerPublicKey = "0xA4faFa5523F63EE58aE7b56ad8EB5a344A19F266";
+export const signerPrivateKey =
+  "854186ae905d9b613063755fbd989f8b148b71f19a2c7deadbea280ac2257e86";
+
+export const infuraProviderHTTPS =
+  "https://goerli.infura.io/v3/b0006a4e470a4e9c8995bc68434726a8";
